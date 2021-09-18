4753bfb975fdc617a4b630c8bb7eba1b78d2862e
TCARVI <tcarvi@me.com>
2021-09-18 08:41:28 -0300

    Implementação de Fault Tolerance Spec

---
+++ src/main/java/com/tcarvi/faulttolerancequarkustemplate/FaulttolerancequarkustemplateRestApplication.java
@@ -0,0 +1,11 @@
+package com.tcarvi.faulttolerancequarkustemplate;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class FaulttolerancequarkustemplateRestApplication extends Application {
+}
